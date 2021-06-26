c2e66ba7efdc674fc44dfeb45c911081d0923309
MateuscMachado <mateuscalabria@gmail.com>
2021-06-26 00:43:17 -0300

    allowing heroku access

--- src/main/java/br/org/serratec/backend/config/AppConfig.java
+++ src/main/java/br/org/serratec/backend/config/AppConfig.java
@@ -10,7 +10,7 @@ public class AppConfig implements WebMvcConfigurer{
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-		.allowedOrigins("http://localhost:3000/")
+		.allowedOrigins("http://localhost:3000/","https://comunistore.herokuapp.com")
 		.allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD")
 		.allowCredentials(true);
 	}
